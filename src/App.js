7091be02308c2423032db05a5a4e0ffb786aec3a
lydiaafiqah <60863233+lydiaafiqah@users.noreply.github.com>
2022-10-07 16:16:39 +0800

    First draft

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './assets/css/App.css'
+
+function App() {
+  return (
+    <div class='sign'>
+      <span class='fast-flicker'>b</span>rea<span class='flicker'>t</span>he
+        <div className='circle-container'>
+					<div className='circle'></div>
+					<div className='circle'></div>
+					<div className='circle'></div>
+				</div>
+    </div>
+  )
+}
+
+export default App
